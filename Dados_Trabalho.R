952f54713c92a3a3c1f721812f6ca4f1355960d1
mrelero <matheuselero1@gmail.com>
2021-08-26 20:27:43 -0300

    Dados_Trabalho

---
+++ Dados_Trabalho.R
@@ -0,0 +1,18 @@
+library(ggplot2)
+data()
+data(package = 'agricolae')
+data(package = 'nlme')
+data(package = 'lme4')
+data(package = 'ggplot2')
+data(package = 'BCA')
+
+
+Dados = "mpg"
+
+help("mpg")
+mpg 
+
+Coleta = data("mpg")
+
+head("mpg")
+mpg 
