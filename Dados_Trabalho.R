0fc5d022ad80a0ac80cf18c5a26bf9646e4456ee
mrelero <matheuselero1@gmail.com>
2021-08-28 10:16:00 -0300

    Seleção de dados e classificação de colunas e variáveis

--- Dados_Trabalho.R
+++ Dados_Trabalho.R
@@ -1,18 +1,37 @@
 library(ggplot2)
 data()
-data(package = 'agricolae')
-data(package = 'nlme')
-data(package = 'lme4')
 data(package = 'ggplot2')
-data(package = 'BCA')
 
 
+#Acesso dos dados de Consumo de combustível de 38 modelos de carros
 Dados = "mpg"
 
 help("mpg")
-mpg 
+mpg  
 
 Coleta = data("mpg")
 
 head("mpg")
 mpg 
+
+#Mapeamento de Variável
+dadosc <- as.data.frame(mpg)
+
+manufacturer <- dadosc$manufacturer
+class(manufacturer)
+class(dadosc$model)
+class(dadosc$displ)
+class(dadosc$year)
+class(dadosc$cyl)
+class(dadosc$trans)
+class(dadosc$cty)
+class(dadosc$hwy)
+class(dadosc$fl)
+class(dadosc$class)
+
+sapply(dadosc, class)
+
+trans <- dadosc$trans
+fl <- dadosc$fl
+
+unique(fl)
